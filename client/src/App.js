43be9d4d01ae06edf9d83c8ea7e86b6c71353691
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-10-31 14:57:53 +0500

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -11,14 +11,13 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className='App-title'>Fib Calculator</h1>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React 3
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
